b4f108a48b16457684135c5161117065bf21433c
Ketyra Sai <Closer2U@firemail.de>
2021-06-04 17:17:52 +0200

    Update calculator.js
    ✔️ output to .innerHTML 
    ❌ wildcardmask() [invert 1s and 0s]
    🔜 functions () {} aufräumen und verbessern --> Effizienz und Dopplungen vermeiden

--- JavaScript/CIDR-calculator/calculator.js
+++ JavaScript/CIDR-calculator/calculator.js
@@ -1,4 +1,7 @@
-//TODO get current IP via API
+// TODO get current IP via API
+// TODO install git 
+// !!! install git
+
 
 function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2){
     /* read all user input Triplets*/
@@ -7,33 +10,56 @@ function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2)
      var thirdTrip = document.getElementById("thirdTriplet").value;
      var fourthTrip = document.getElementById("fourthTriplet").value;
         
-     /* join Triplets to ip */
+     /* ===== join Triplets to ip ===== */
          var ip = firstTrip + "." + secondTrip + "." + thirdTrip + "." + fourthTrip;
          console.log("Concated ip: " + ip);
  
+    
+    //==========================
+    //======= INPUT ============
+
+     // ======= User input how many Computers need to be joined? =====
      var hostAmount = document.getElementById("hostAmount2").value;
          console.log("How many hosts? (userInput) " + hostAmount);
- 
-     // Number of hosts to accommodate --> calculate CIDR table SUFFIX
+         // !!! display error message if forgot to fill 
+         // TODO display error message if forgot to fill 
+         if (hostAmount == ""){
+             alert("Please enter number of required hosts!")
+         }
+    
+    
+    //==========================
+    //======= OUTPUT ===========
+
+     // ===== Number of hosts to accommodate --> calculate CIDR table SUFFIX
      var suffix = Math.ceil(Math.log2(hostAmount));
+        // 
+        document.getElementById("suffix").placeholder = suffix;
+            console.log("suffix .innerHTML: " + document.getElementById("suffix").innerHTML.valueOf());
+
+                // ==== trying to solve problem to write into INPUT field
+                // let suffixtest = document.getElementById("suffix").innerHTML.valueOf();
+                // document.getElementById("suffix").innerHTML = suffixtest;
+                // console.log("suffixtest: " + suffixtest);
+
          console.log("suffix: " + suffix);
          //TODO output into html  --> /suffix
  
      var netBit = suffix;
          console.log("Network Bit: " + netBit);
-         //TODO output into html 
+         document.getElementById("networkBits").innerHTML = netBit;
  
      var hostBit = 32 - netBit;
          console.log("Host Bits: " + hostBit);
-         //TODO output into html 
+         document.getElementById("hostBits").innerHTML = hostBit;
  
      var maxSubnets = Math.pow(2, netBit);
          console.log("maximum Subnets in Network: " + maxSubnets);
-         //TODO this is WRONG! -- must be immensively high number;
+         document.getElementById("subnetMax").innerHTML = maxSubnets;
  
      var maxHosts = Math.pow(2, hostBit);
          console.log("Max hosts per subnet with given suffix: " + maxHosts);
-         //TODO output into html 
+         document.getElementById("hostMax").innerHTML = maxHosts;
  
     //  var a = netBit.toString(2);
     //      b = a << 8;
@@ -42,7 +68,7 @@ function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2)
 
 
 
-
+        var subnetmaskBinarySplit;
      var subnetmask = function() {
         var subnetmaskHelper = '';
         for (var i=0; i<hostBit; i++){
@@ -51,7 +77,7 @@ function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2)
         console.log("subnetmaskHelper before BitShift: " + subnetmaskHelper);
 
                
-        //~~~~~~~~~~~~ concat the 0s up to 32
+        // ====== concat the 0s up to 32 =====
         console.log(subnetmaskHelper.length)
         for (j=subnetmaskHelper.length; j < 32; j++){
             subnetmaskHelper = subnetmaskHelper + 0;
@@ -59,18 +85,18 @@ function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2)
           console.log("concated String: " + subnetmaskHelper);
 
 
-        // ~~~~~~~ split into octets --> not yet awesome... 
+        // ========== split into octets --> not yet awesome... ======
         subnetmaskBinary = subnetmaskHelper.toString(2).match(/.{1,8}/g);
             console.log("Octets: " + subnetmaskBinary);
         subnetmaskBinarySplit = subnetmaskBinary.toString().split(',');
         // XXX returns ARRAY!!!!!
            console.table(subnetmaskBinarySplit);
 
-        //~~~~~~~~~ join binary array with '.'
+        // ======== join binary array with '.'
             // subnetmaskBinaryFinal = subnetmaskBinarySplit.join('.');
             // console.log(subnetmaskBinaryFinal);
 
-        //~~~~~~~~~~~ convert binaries to decimal
+        // ========= convert binaries to decimal
         // https://stackoverflow.com/questions/9329446/for-each-over-an-array-in-javascript
         var index;
         var arr = [];
@@ -82,12 +108,22 @@ function calculate(firstTrip , secondTrip , thirdTrip , fourthTrip, hostAmount2)
         console.table(arr);
         var subnetmaskFinal = arr.join('.');
         console.log(subnetmaskFinal);
-        //TODO output subnetmaskFinal to HTML 
-        };
+        document.getElementById("subnetmask").innerHTML = subnetmaskFinal;
+    };
         subnetmask();
+            
 
+    //TODO make wildcard function to invert numbers
+    var wildcardmask = function (){
+        //TODO invert subnetmasks 1s and 0s
+        // ??? how to work with array here?
+        subnetmaskBinarySplit[0] = (subnetmaskBinarySplit ? 0 : 1);
+        subnetmaskBinarySplit[0] = +!subnetmaskBinarySplit;
+        console.log("Wildcard inverted: " + subnetmaskBinarySplit[0]);
+    };
+        wildcardmask();
 
- 
+    
         // function dec2bin(dec) {
         //     return (dec >>> 0).toString(2);
         //   }
